79a65a8ff8060da7f3390760651478d1818369b4
Ashlyn Barrett <ashbarrett@my.365.bellevue.edu>
2023-07-20 20:32:26 -0500

    module_7

--- module_7/db_init.sql
+++ module_7/db_init.sql
@@ -19,7 +19,7 @@ GRANT ALL PRIVILEGES ON pysports.* TO'pysports_user'@'localhost';
 -- drop tables if they are present
 DROP TABLE IF EXISTS player;
 DROP TABLE IF EXISTS team;
-
+x
 
 -- create the team table 
 CREATE TABLE team (
